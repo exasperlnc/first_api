c5b306bd73f7dd3d622963d95859a8e70ef4ec98
Logan Cole <emtlogancole@gmail.com>
2023-04-28 14:30:33 -0500

    add popularity to books

--- app/controllers/api/v2/books_controller.rb
+++ app/controllers/api/v2/books_controller.rb
@@ -1,5 +1,15 @@
 class Api::V2::BooksController < ApplicationController
+  before_save { |book| book.popularity = caluculate_popularity }
   def index
     render json: Book.all
+  end
+
+  private
+  def caluculate_popularity
+    if number_sold > 5
+      'high'
+    else
+      'low'
+    end
   end
 end
